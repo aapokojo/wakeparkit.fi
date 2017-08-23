975cb9a63fcb1c348243346ccec05e0c543b5b42
Aapo Kojo <aapo.kojo@reaktor.fi>
2017-08-23 09:34:58 +0300

    Move all data to data.json

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -5,6 +5,19 @@ fetch('/data.json')
 .then(function(data) {
 	$.each( data.wakeparkit, function( i, val ) {
 
-	$( "#container" ).append( "<div class=\"park\"><img src=\"https://maps.googleapis.com/maps/api/staticmap?center=60.1715187,24.9030484&zoom=15&size=300x300&maptype=roadmap&markers=color:red%7Clabel:W%7C60.1715187,24.9013484&key=AIzaSyDGv-c2sGWtBXT47oimKVSGKBWRAyd84mU\"><div class=\"info\"><div class=\"park_title\">Wakespot</div><div class=\"park_url\"><a href=\"http://www.wakespot.fi\">wakespot.fi</a></div></div></div>");
+	$( "#container" ).append("<div class=\"park\">"
+    + "<img src=\"https://maps.googleapis.com/maps/api/staticmap?center="
+    + data.wakeparkit[i].lat
+    + ","
+    + data.wakeparkit[i].lon
+    + "&zoom=15&size=300x300&maptype=roadmap"
+    + "&markers=color:red%7Clabel:" + data.wakeparkit[i].key + "%7C"
+    + data.wakeparkit[i].pin_lat
+    + ","
+    + data.wakeparkit[i].pin_lon
+    + "&key=AIzaSyDGv-c2sGWtBXT47oimKVSGKBWRAyd84mU"
+    + "\"><div class=\"info\">"
+    + "<div class=\"park_title\">" + data.wakeparkit[i].name + "</div>"
+    + "<div class=\"park_url\"><a href=\"" + data.wakeparkit[i].url + "\">&rarr;</a></div></div></div>");
 	});
 });
