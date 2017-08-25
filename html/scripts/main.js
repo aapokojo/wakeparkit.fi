a24e89964132bdb801452061374362f184d87a12
Aapo Kojo <aapo.kojo@reaktor.fi>
2017-08-25 14:21:56 +0300

    Modify styles

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -15,7 +15,8 @@ fetch('/data.json')
     + data.wakeparkit[i].pin_lat
     + ","
     + data.wakeparkit[i].pin_lon
-    + "&key=AIzaSyDGv-c2sGWtBXT47oimKVSGKBWRAyd84mU"
+    + "&style=feature:road.local%7Celement:geometry%7Ccolor:0xffffff&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x111111&style=element:labels%7Cinvert_lightness:true&style=feature:road.arterial%7Celement:labels%7Cinvert_lightness:false"
+    + "&key=AIzaSyAtUNkgUu1-VfzdrS5GN_XMggHw3SFYjEk"
     + "\"><div class=\"info\">"
     + "<div class=\"park_title\">" + data.wakeparkit[i].name + "</div>"
     + "<div class=\"park_url\"><a href=\"" + data.wakeparkit[i].url + "\">www</a></div></div></div>");
